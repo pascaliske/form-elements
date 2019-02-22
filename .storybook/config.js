bb36a04a1136af307b3debee4e186379f84d64ca
Pascal Iske <info@pascal-iske.de>
2019-02-22 09:23:39 +0100

    feat(storybook): add module dependencies for all stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,9 @@
-import { configure, addDecorator } from '@storybook/angular'
+import { configure, addDecorator, moduleMetadata } from '@storybook/angular'
 import { themes } from '@storybook/components'
 import { withNotes } from '@storybook/addon-notes'
 import { withOptions } from '@storybook/addon-options'
+import { ReactiveFormsModule } from '@angular/forms'
+import { FormElementsModule } from '../projects/form-elements/src/lib/form-elements.module'
 import { name, repository } from '../package.json'
 
 const load = require.context('../projects', true, /.stories.ts$/)
@@ -13,6 +15,16 @@ addDecorator(
         url: repository.url.replace(/.git$/, ''),
         theme: themes.normal,
         hierarchySeparator: /\//,
+    }),
+)
+addDecorator(
+    moduleMetadata({
+        imports: [
+            ReactiveFormsModule,
+            FormElementsModule.forRoot({
+                datepicker: true,
+            }),
+        ],
     }),
 )
 
