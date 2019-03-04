dac7523d24de990c41c7fee3288643fde15f7e9e
Pascal Iske <info@pascal-iske.de>
2019-03-04 23:47:47 +0100

    refactor(styles): update scss bundling for style distribution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
     public options: Array<FSelectOption> = [
