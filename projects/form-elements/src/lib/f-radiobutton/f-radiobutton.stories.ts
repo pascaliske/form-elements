5ec5a0410ee78cf247901de2b177a14239276838
Pascal Iske <info@pascal-iske.de>
2019-04-01 19:58:40 +0200

    feat(f-radiobutton): add stories for f-radiobutton component and optimize styles

---
+++ projects/form-elements/src/lib/f-radiobutton/f-radiobutton.stories.ts
@@ -0,0 +1,48 @@
+import { storiesOf } from '@storybook/angular'
+import { text } from '@storybook/addon-knobs'
+
+import { FRadiobuttonComponent } from './f-radiobutton.component'
+import FRadiobuttonReadme from './f-radiobutton.readme.md'
+import '../main.scss'
+
+storiesOf('Basic|F-Radiobutton', module)
+    .addParameters({ notes: FRadiobuttonReadme })
+    .add('Basic', () => ({
+        component: FRadiobuttonComponent,
+        props: {
+            name: text('name', 'name'),
+            label: text('label', 'Label'),
+            options: [
+                {
+                    label: 'Foo',
+                    value: 'foo',
+                    checked: true,
+                },
+                {
+                    label: 'Bar',
+                    value: 'bar',
+                    checked: false,
+                },
+            ],
+        },
+    }))
+    .add('Disabled', () => ({
+        component: FRadiobuttonComponent,
+        props: {
+            name: text('name', 'name'),
+            label: text('label', 'Label'),
+            options: [
+                {
+                    label: 'Foo',
+                    value: 'foo',
+                    checked: true,
+                },
+                {
+                    label: 'Bar',
+                    value: 'bar',
+                    checked: false,
+                },
+            ],
+            disabled: true,
+        },
+    }))
