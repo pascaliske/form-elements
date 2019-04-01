38c417b89b0874e16e954dd77d50fec507e9fd77
Pascal Iske <info@pascal-iske.de>
2019-04-01 19:54:06 +0200

    feat(f-row): add stories for f-row component and optimize styles

---
+++ projects/form-elements/src/lib/f-row/f-row.readme.md
@@ -0,0 +1,11 @@
+# F-Row
+
+## Usage
+
+```html
+<cmp-f-row [layout]="'1-1'">
+    <cmp-f-column>
+        <cmp-f-button [label]="'Label'"></cmp-f-button>
+    </cmp-f-column>
+</cmp-f-row>
+```
---
+++ projects/form-elements/src/lib/f-row/f-row.stories.ts
@@ -0,0 +1,67 @@
+import { storiesOf } from '@storybook/angular'
+
+import FRowReadme from './f-row.readme.md'
+import '../main.scss'
+
+storiesOf('Layout|F-Row', module)
+    .addParameters({ notes: FRowReadme })
+    .add('Layout 1', () => ({
+        template: `
+            <cmp-f-row [layout]="'1'">
+                <cmp-f-column>
+                    <cmp-f-input [label]="'Label'"></cmp-f-input>
+                </cmp-f-column>
+            </cmp-f-row>
+        `,
+    }))
+    .add('Layout 1-1', () => ({
+        template: `
+            <cmp-f-row [layout]="'1-1'">
+                <cmp-f-column>
+                    <cmp-f-input [label]="'Label'"></cmp-f-input>
+                </cmp-f-column>
+                <cmp-f-column>
+                    <cmp-f-input [label]="'Label'"></cmp-f-input>
+                </cmp-f-column>
+            </cmp-f-row>
+        `,
+    }))
+    .add('Layout 1-1-1', () => ({
+        template: `
+            <cmp-f-row [layout]="'1-1-1'">
+                <cmp-f-column>
+                    <cmp-f-input [label]="'Label'"></cmp-f-input>
+                </cmp-f-column>
+                <cmp-f-column>
+                    <cmp-f-input [label]="'Label'"></cmp-f-input>
+                </cmp-f-column>
+                <cmp-f-column>
+                    <cmp-f-input [label]="'Label'"></cmp-f-input>
+                </cmp-f-column>
+            </cmp-f-row>
+        `,
+    }))
+    .add('Layout 1-2', () => ({
+        template: `
+            <cmp-f-row [layout]="'1-2'">
+                <cmp-f-column>
+                    <cmp-f-input [label]="'Label'"></cmp-f-input>
+                </cmp-f-column>
+                <cmp-f-column>
+                    <cmp-f-input [label]="'Label'"></cmp-f-input>
+                </cmp-f-column>
+            </cmp-f-row>
+        `,
+    }))
+    .add('Layout 2-1', () => ({
+        template: `
+            <cmp-f-row [layout]="'2-1'">
+                <cmp-f-column>
+                    <cmp-f-input [label]="'Label'"></cmp-f-input>
+                </cmp-f-column>
+                <cmp-f-column>
+                    <cmp-f-input [label]="'Label'"></cmp-f-input>
+                </cmp-f-column>
+            </cmp-f-row>
+        `,
+    }))
