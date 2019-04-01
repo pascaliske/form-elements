65f1598b9b9b6a50405ab29ecdd55e0c76540dc0
Pascal Iske <info@pascal-iske.de>
2019-04-01 19:53:34 +0200

    feat(f-column): add stories for f-column component and optimize styles

---
+++ projects/form-elements/src/lib/f-column/f-column.stories.ts
@@ -0,0 +1,25 @@
+import { storiesOf } from '@storybook/angular'
+
+import FColumnReadme from './f-column.readme.md'
+import '../main.scss'
+
+storiesOf('Layout|F-Column', module)
+    .addParameters({ notes: FColumnReadme })
+    .add('Align Left', () => ({
+        template: `
+            <cmp-f-row>
+                <cmp-f-column [flex]="true" [align]="'left'">
+                    <cmp-f-button [label]="'Label'"></cmp-f-button>
+                </cmp-f-column>
+            </cmp-f-row>
+        `,
+    }))
+    .add('Align Right', () => ({
+        template: `
+            <cmp-f-row>
+                <cmp-f-column [flex]="true" [align]="'right'">
+                    <cmp-f-button [label]="'Label'"></cmp-f-button>
+                </cmp-f-column>
+            </cmp-f-row>
+        `,
+    }))
