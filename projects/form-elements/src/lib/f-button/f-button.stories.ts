c1c68199a361950f1fd7964d1608a886b2157ebe
Pascal Iske <info@pascal-iske.de>
2019-06-17 23:13:59 +0200

    fix(f-button): fix icon prop knob for button stories

--- projects/form-elements/src/lib/f-button/f-button.stories.ts
+++ projects/form-elements/src/lib/f-button/f-button.stories.ts
@@ -1,13 +1,13 @@
 import { storiesOf } from '@storybook/angular'
 import { radios, text, boolean } from '@storybook/addon-knobs'
 import { action } from '@storybook/addon-actions'
-import * as faIcons from '@fortawesome/free-solid-svg-icons'
+import { faCheck, faAngleDown, faPaperPlane, faSave } from '@fortawesome/free-solid-svg-icons'
 
 import { FButtonComponent } from './f-button.component'
 import FButtonReadme from './f-button.readme.md'
 import '../main.scss'
 
-const icons = ['faCheck', 'faAngleDown', 'faPaperPlane', 'faSave']
+const icons = { faCheck, faAngleDown, faPaperPlane, faSave }
 
 storiesOf('Basic|F-Button', module)
     .addParameters({ notes: FButtonReadme })
@@ -17,7 +17,7 @@ storiesOf('Basic|F-Button', module)
             type: radios('type', { button: 'button', submit: 'submit' }, 'button'),
             id: text('id', 'primary'),
             label: text('label', 'Primary Button'),
-            icon: faIcons[radios('icon', icons, icons[0])],
+            icon: icons[radios('icon', icons, icons[0])],
             theme: 'primary',
             disabled: boolean('disabled', false),
             clicked: action('click'),
@@ -29,7 +29,7 @@ storiesOf('Basic|F-Button', module)
             type: radios('type', { button: 'button', submit: 'submit' }, 'button'),
             id: text('id', 'secondary'),
             label: text('label', 'Secondary Button'),
-            icon: faIcons[radios('icon', icons, icons[0])],
+            icon: icons[radios('icon', icons, icons[0])],
             theme: 'secondary',
             disabled: boolean('disabled', false),
             clicked: action('click'),
