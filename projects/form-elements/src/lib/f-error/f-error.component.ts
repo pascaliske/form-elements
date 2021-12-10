adf37c022ccc80ce1cfdf009f2a3d1ee52c7b1ef
Pascal Iske <info@pascaliske.dev>
2021-12-11 00:18:06 +0100

    fix(f-error): use type-only import for validation type

--- projects/form-elements/src/lib/f-error/f-error.component.ts
+++ projects/form-elements/src/lib/f-error/f-error.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core'
 import type { AbstractControl } from '@angular/forms'
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core'
 import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons'
-import { FValidation } from '../f-input/f-input.component'
+import type { FValidation } from '../f-input/f-input.component'
 
 @Component({
     selector: 'cmp-f-error',
